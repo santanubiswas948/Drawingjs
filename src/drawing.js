de5141d6a5f9ad417bd573bd4a396bc1d3c2d35b
Santanu Biswas <santanubiswas1234567@gmail.com>
2018-03-11 00:38:20 +0530

    Change in Code

--- src/drawing.js
+++ src/drawing.js
@@ -7,10 +7,14 @@ document.onreadystatechange = () =>
 				prev_x: 0,
 				prev_y :0,
 				flag : 0,
+				flag1 : 0,
 				start : function()
 				{
 					this.canvas=document.getElementById('canvas');
 					this.ctx=canvas_elm.canvas.getContext("2d");
+					this.array_pointsX=[];
+				    this.array_pointsY=[];
+					var count_mouse_down=0;
 					this.canvas.addEventListener('mousedown',function(){
 						if(canvas_elm.flag==0)
 						{
@@ -18,15 +22,14 @@ document.onreadystatechange = () =>
 								canvas_elm.canvas.addEventListener('mousemove',draw);
 						}
 					});
-					this.canvas.addEventListener('click',function(){
-						if(canvas_elm.flag==1)
-						{
-							canvas_elm.canvas.removeEventListener('mousemove',draw);
-							canvas_elm.flag=0;
-							canvas_elm.prev_x=0;
-							canvas_elm.prev_y=0;
-						}	
-					});
+					canvas_elm.canvas.addEventListener('mouseup',function(){
+								canvas_elm.array_pointsX=[];
+								canvas_elm.array_pointsY=[];
+								canvas_elm.canvas.removeEventListener('mousemove',draw);
+								canvas_elm.flag=0;
+								canvas_elm.prev_x=0;
+								canvas_elm.prev_y=0;
+						});
 				},
 				clear : function()
 				{
@@ -39,24 +42,35 @@ document.onreadystatechange = () =>
 				var x=evt.pageX-getPositionX(canvas_elm.canvas);
 				var y=evt.pageY-getPositionY(canvas_elm.canvas);
 				
-				if(canvas_elm.prev_x==0 && canvas_elm.prev_y==0)
+				if(canvas_elm.flag1==0)
 				{
-					canvas_elm.ctx.fillStyle="#000";
-					canvas_elm.ctx.fillRect(x,y,2,2);
+					canvas_elm.flag1=1;
+					canvas_elm.array_pointsX.push(x);
+					canvas_elm.array_pointsY.push(y);
 				}
 				else
 				{
-					canvas_elm.ctx.beginPath();
-					canvas_elm.ctx.moveTo(canvas_elm.prev_x,canvas_elm.prev_y);
-					canvas_elm.ctx.lineTo(x,y);
-					canvas_elm.ctx.lineWidth=2;
-					canvas_elm.ctx.strokeStyle="5px solid #000";
-					canvas_elm.ctx.stroke();
-					canvas_elm.ctx.closePath();
+					canvas_elm.flag=1;
+					canvas_elm.array_pointsX.push(x);
+					canvas_elm.array_pointsY.push(y);
+					for(var i=1;i<canvas_elm.array_pointsX.length;i++)
+						{
+							var prevX=canvas_elm.array_pointsX[i-1];
+							var prevY=canvas_elm.array_pointsY[i-1];
+							var currX=canvas_elm.array_pointsX[i];
+							var currY=canvas_elm.array_pointsY[i];
+							canvas_elm.ctx.beginPath();
+							canvas_elm.ctx.moveTo(prevX,prevY);
+							canvas_elm.ctx.lineTo(currX,currY);
+							canvas_elm.ctx.lineWidth=2.3;
+							canvas_elm.ctx.strokeStyle="5px solid #000";
+							canvas_elm.ctx.stroke();
+							canvas_elm.ctx.closePath();
+							
+						}
+					canvas_elm.array_pointsX.push(x);
+					canvas_elm.array_pointsY.push(y);
 				}
-				canvas_elm.prev_x=x;
-				canvas_elm.prev_y=y;
-				canvas_elm.flag=1;
 			}
 
 //For getting X position-----------------
